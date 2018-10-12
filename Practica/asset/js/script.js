924c193171fc5c232cd2c2d9b04fcc1a59341b6e
kaztro <00229017@uca.edu.sv>
2018-10-12 10:11:03 -0600

    comienzo

---
+++ Practica/asset/js/script.js
@@ -0,0 +1,20 @@
+let container = document.getElementById("c1");
+/*¿Qué es lo que contiene la variable container? 
+Contiene un div
+*/
+container.innerText = "¡Hola mundo!";
+/* ¿Qué visualiza en el navegador? 
+En la primera caja naranja se coloco el ¡Hola mundo! 
+*/
+
+container.style.backgroundColor = "#000000";
+
+container.style.color = "#1cb723";
+
+container.style.height = "200px";
+
+container.style.width = "200px";
+
+
+
+
